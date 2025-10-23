d8d9495a269d560dba9a9296c25a52b9eee26432
Kirk Baskin <ksba241@csr.uky.edu>
2025-10-23 09:56:00 -0400

    sh file

---
+++ run/explore_data.sh
@@ -0,0 +1,14 @@
+#!/bin/bash                                                                     
+#SBATCH --time=72:00:00 # Time limit for the job (REQUIRED).                    
+#SBATCH --job-name=explore-data # Job name                                       
+#SBATCH --ntasks=1 # Number of cores for the job. Same as SBA TCH -n 1          
+#SBATCH --partition=V4V32_SKY32M192_L # Partition/queue to run the job in. (REQUIRED)                                                                          
+#SBATCH -e explore-data.err # Error file for this job.                              
+#SBATCH -o explore-data.out # Output file for this job.                             
+#SBATCH -A gol_hkh232_uksr # Project allocation account name (REQUIRED)         
+#SBATCH --gres=gpu:1                                                            
+
+module load ccs/Miniconda3
+eval "$(conda shell.bash hook)"
+source activate myenv
+python src/explore-data.py
