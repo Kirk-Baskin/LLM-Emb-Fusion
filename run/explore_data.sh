d0df937c6e3631d89148dce035844e9d1c373eed
Kirk Baskin <ksba241@csr.uky.edu>
2025-10-23 09:58:54 -0400

    diff partition

--- run/explore_data.sh
+++ run/explore_data.sh
@@ -2,7 +2,7 @@
 #SBATCH --time=72:00:00 # Time limit for the job (REQUIRED).                    
 #SBATCH --job-name=explore-data # Job name                                       
 #SBATCH --ntasks=1 # Number of cores for the job. Same as SBA TCH -n 1          
-#SBATCH --partition=V4V32_SKY32M192_L # Partition/queue to run the job in. (REQUIRED)                                                                          
+#SBATCH --partition=P4V16_HAS16M128_L # Partition/queue to run the job in. (REQUIRED)                                                                          
 #SBATCH -e explore-data.err # Error file for this job.                              
 #SBATCH -o explore-data.out # Output file for this job.                             
 #SBATCH -A gol_hkh232_uksr # Project allocation account name (REQUIRED)         
